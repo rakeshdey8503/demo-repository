b0b3328d7209719fb738203b07ff1e5e9344cb72
RAKESHDEY8503 <RAKESH.DEY@ICLOUD.COM>
2026-03-26 21:09:06 -0400

    commit

--- calculator.py
+++ calculator.py
@@ -10,8 +10,8 @@ def multiply(x, y):  # New function
     """Multiplies two numbers and returns the result."""
     return x * y
 
-def divide(a, b):
-    return a / b
+#def divide(a, b):
+#    return a / b
 
 # New feature: Power function
 def power(a, b):
